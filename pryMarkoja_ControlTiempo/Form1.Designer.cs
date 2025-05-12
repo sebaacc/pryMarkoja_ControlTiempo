0bb678122878a0a1ce215769c26795ab2e0fce48
seba <sebacc11@gmail.com>
2025-05-12 13:19:06 -0300

    first commit

---
+++ pryMarkoja_ControlTiempo/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace pryMarkoja_ControlTiempo
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sBarraInferior = new System.Windows.Forms.StatusStrip();
+            this.lblTiempoStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tTiempoAplicacion = new System.Windows.Forms.Timer(this.components);
+            this.btnTrabajo = new System.Windows.Forms.Button();
+            this.sBarraInferior.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sBarraInferior
+            // 
+            this.sBarraInferior.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblTiempoStatus});
+            this.sBarraInferior.Location = new System.Drawing.Point(0, 428);
+            this.sBarraInferior.Name = "sBarraInferior";
+            this.sBarraInferior.Size = new System.Drawing.Size(800, 22);
+            this.sBarraInferior.TabIndex = 0;
+            this.sBarraInferior.Text = "statusStrip1";
+            // 
+            // lblTiempoStatus
+            // 
+            this.lblTiempoStatus.Name = "lblTiempoStatus";
+            this.lblTiempoStatus.Size = new System.Drawing.Size(114, 17);
+            this.lblTiempoStatus.Text = "Tiempo transcurrido";
+            // 
+            // tTiempoAplicacion
+            // 
+            this.tTiempoAplicacion.Tick += new System.EventHandler(this.tTiempoAplicacion_Tick);
+            // 
+            // btnTrabajo
+            // 
+            this.btnTrabajo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTrabajo.Location = new System.Drawing.Point(238, 176);
+            this.btnTrabajo.Name = "btnTrabajo";
+            this.btnTrabajo.Size = new System.Drawing.Size(297, 60);
+            this.btnTrabajo.TabIndex = 1;
+            this.btnTrabajo.Text = "Trabajo";
+            this.btnTrabajo.UseVisualStyleBackColor = true;
+            this.btnTrabajo.Click += new System.EventHandler(this.btnTrabajo_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTrabajo);
+            this.Controls.Add(this.sBarraInferior);
+            this.Name = "frmLogin";
+            this.Text = "Inicio Sesión";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.sBarraInferior.ResumeLayout(false);
+            this.sBarraInferior.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip sBarraInferior;
+        private System.Windows.Forms.Timer tTiempoAplicacion;
+        private System.Windows.Forms.Button btnTrabajo;
+        private System.Windows.Forms.ToolStripStatusLabel lblTiempoStatus;
+    }
+}
+
---
+++ pryMarkoja_ControlTiempo/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Hosting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryMarkoja_ControlTiempo
+{
+    public partial class frmLogin : Form
+    {
+        private System.Diagnostics.Stopwatch miCronometro = new System.Diagnostics.Stopwatch();
+        private bool cronometroActivo = false;
+
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tTiempoAplicacion_Tick(object sender, EventArgs e)
+        {
+            TimeSpan tiempoTranscurrido = miCronometro.Elapsed;
+            string tiempoFormateado = string.Format("{0:00}:{1:00}:{2:00}",
+                tiempoTranscurrido.Hours, tiempoTranscurrido.Minutes, tiempoTranscurrido.Seconds);
+            lblTiempoStatus.Text = tiempoFormateado;
+        }
+
+        private void btnTrabajo_Click(object sender, EventArgs e)
+        {
+            activarCronometro();
+        }
+
+        private void activarCronometro()
+        {
+            if (cronometroActivo == false)
+            {
+                cronometroActivo = true;
+                tTiempoAplicacion.Start();
+                btnTrabajo.Text = "Finalizar";
+            } else
+            {
+                cronometroActivo = false;
+                tTiempoAplicacion.Start();
+                btnTrabajo.Text = "Trabajo";
+            }
+        }
+    }
+}
